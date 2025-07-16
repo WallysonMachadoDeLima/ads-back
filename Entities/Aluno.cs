f068455723b8cc8204caf5a60bec2ded3553b1e1
Rafael Felipe <rafaguiar2005@gmail.com>
2025-07-15 22:01:18 -0400

    feat: Refactor Aluno module to use Ads namespace; add CPF validation in AlunoService

--- Entities/Aluno.cs
+++ Entities/Aluno.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
-namespace CrudVeiculos.Entities
+namespace Ads.Entities
 {
     [Table("Aluno")]
     public class Aluno
